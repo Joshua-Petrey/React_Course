32bb3ce19374207945dcec77b51cf2518b45c3e9
Joshua-Petrey <Joshua-Petrey@users.noreply.github.com>
2021-05-19 11:17:49 -0400

    Created, styled, and rendered first component.

--- section-2/src/App.js
+++ section-2/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <h2>Lets get started!!!</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
 }
 
 export default App;
