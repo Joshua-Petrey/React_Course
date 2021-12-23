f1284475837ccd077cb44f05a76bc1e0d7c30d8a
Joshua Petrey <54009418+Joshua-Petrey@users.noreply.github.com>
2021-12-23 16:53:11 -0500

    Finished first part of section

--- section-16-forms/src/components/SimpleInput.js
+++ section-16-forms/src/components/SimpleInput.js
@@ -1,12 +1,61 @@
+import { useState} from 'react';
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  // has input been touched
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+  // derived from enteredName state
+  const enteredNameIsValid = enteredName.trim() !== "";
+  // if input is invalid and form has been touched
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+  const nameInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+  let formIsValid = false
+
+// check if all form inputs are valid
+  if(enteredNameIsValid){
+    formIsValid = true
+  } 
+  // set state to input value on every keystroke
+  const nameInputChangeHandler = (e) => {
+    setEnteredName(e.target.value);
+  };
+  // if form lost focus
+  const nameInputBlurHandler = (e) => {
+    setEnteredNameTouched(true);
+  };
+
+  const formSubmissionHandler = (e) => {
+    e.preventDefault();
+    setEnteredNameTouched(true);
+
+    if (!enteredNameIsValid) {
+      return;
+    }
+    // reset form
+    setEnteredName("");
+    setEnteredNameTouched(false)
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          value={enteredName}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          onBlur={nameInputBlurHandler}
+        />
+        {/* User error feedback */}
+        {nameInputIsInvalid && (
+          <p className="error-text">Name must not be empty</p>
+        )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid} >Submit</button>
       </div>
     </form>
   );
