d576b88dabd462a12e2aa624d23e434c36db2bb1
Joshua Petrey <54009418+Joshua-Petrey@users.noreply.github.com>
2022-01-11 10:07:45 -0500

    Added lazy loading

--- section-20-react-router/practice-project/src/App.js
+++ section-20-react-router/practice-project/src/App.js
@@ -1,35 +1,50 @@
 import { Route, Routes, Navigate, Link } from "react-router-dom";
-
-import AllQuotes from "./pages/AllQuotes";
-import QuoteDetails from "./pages/QuoteDetails";
-import NewQuote from "./pages/NewQuote";
-import NotFound from "./pages/NotFound";
 import Layout from "./components/layout/Layout";
 import Comments from "./components/comments/Comments";
+import react, {Suspense} from "react";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+const AllQuotes = react.lazy(() => {
+  import("./pages/AllQuotes");
+});
+
+const NewQuote = react.lazy(() => {
+  import('./pages/NewQuote')
+})
+
+const QuoteDetails = react.lazy(() => {
+  import("./pages/QuoteDetails");
+})
+
+const NotFound = react.lazy(() => {
+  import("./pages/NotFound");
+})
 
 function App() {
   return (
     <Layout>
-      <Routes>
-        <Route path="/" element={<Navigate replace to="/quotes" />} />
-        <Route path="/quotes" element={<AllQuotes />} />
-        <Route path="/quote/:quoteId" element={<QuoteDetails />}>
-          {/* Nested route */}
-          <Route
-            path=""
-            element={
-              <div className="centered">
-                <Link className="btn--flat" to={`comments`}>
-                  Load Comments
-                </Link>
-              </div>
-            }
-          />
-          <Route path={`comments`} element={<Comments />} />
-        </Route>
-        <Route path="/new-quote" element={<NewQuote />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense fallback={<div className="centered"><LoadingSpinner/></div>}>
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/quotes" />} />
+          <Route path="/quotes" element={<AllQuotes />} />
+          <Route path="/quote/:quoteId" element={<QuoteDetails />}>
+            {/* Nested route */}
+            <Route
+              path=""
+              element={
+                <div className="centered">
+                  <Link className="btn--flat" to={`comments`}>
+                    Load Comments
+                  </Link>
+                </div>
+              }
+            />
+            <Route path={`comments`} element={<Comments />} />
+          </Route>
+          <Route path="/new-quote" element={<NewQuote />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </Layout>
   );
 }
